5969afcf53edcd7c27a8aae8caa7407067053a4b
hiraq <hiraq@onebitmedia.com>
2016-09-05 13:36:33 +0700

    Change initiate log new object, now using init function

--- log.go
+++ log.go
@@ -12,6 +12,15 @@ var (
 	CustomOutput io.Writer
 )
 
+func init() {
+	Logger = log.New(os.Stdout, "", 0)
+}
+
+//NewCustomOutput used to change how Logger send log message
+func NewCustomOutput(output io.Writer) {
+	Logger.SetOutput(output)
+}
+
 //NewCustomeLogger used to set custom log output destination
 func NewCustomeLogger(std, custom io.Writer) *log.Logger {
 	logger := log.New(io.MultiWriter(std, custom), "", 0)
@@ -21,49 +30,49 @@ func NewCustomeLogger(std, custom io.Writer) *log.Logger {
 //Alert used to create log message with alert level
 func Alert(msg string, options ...Option) {
 	message := buildMessage(LevelAlert, msg, options)
-	Logger = NewCustomeLogger(os.Stderr, CustomOutput)
+	NewCustomOutput(io.MultiWriter(os.Stderr, CustomOutput))
 	Logger.Println(string(message))
 }
 
 //Info used to create log message with info level
 func Info(msg string, options ...Option) {
 	message := buildMessage(LevelInfo, msg, options)
-	Logger = NewCustomeLogger(os.Stdout, CustomOutput)
+	NewCustomOutput(io.MultiWriter(os.Stdout, CustomOutput))
 	Logger.Println(string(message))
 }
 
 //Critical used to create log message with critical level
 func Critical(msg string, options ...Option) {
 	message := buildMessage(LevelCritical, msg, options)
-	Logger = NewCustomeLogger(os.Stderr, CustomOutput)
+	NewCustomOutput(io.MultiWriter(os.Stderr, CustomOutput))
 	Logger.Println(string(message))
 }
 
 //Error used to create log message with error level
 func Error(msg string, options ...Option) {
 	message := buildMessage(LevelError, msg, options)
-	Logger = NewCustomeLogger(os.Stderr, CustomOutput)
+	NewCustomOutput(io.MultiWriter(os.Stderr, CustomOutput))
 	Logger.Println(string(message))
 }
 
 //Warn used to create log message with warning level
 func Warn(msg string, options ...Option) {
 	message := buildMessage(LevelWarning, msg, options)
-	Logger = NewCustomeLogger(os.Stdout, CustomOutput)
+	NewCustomOutput(io.MultiWriter(os.Stdout, CustomOutput))
 	Logger.Println(string(message))
 }
 
 //Notice used to create log message with warning level
 func Notice(msg string, options ...Option) {
 	message := buildMessage(LevelNotice, msg, options)
-	Logger = NewCustomeLogger(os.Stdout, CustomOutput)
+	NewCustomOutput(io.MultiWriter(os.Stdout, CustomOutput))
 	Logger.Println(string(message))
 }
 
 //Debug used to create log message with warning level
 func Debug(msg string, options ...Option) {
 	message := buildMessage(LevelDebug, msg, options)
-	Logger = NewCustomeLogger(os.Stdout, CustomOutput)
+	NewCustomOutput(io.MultiWriter(os.Stdout, CustomOutput))
 	Logger.Println(string(message))
 }
 
